526c11cd49b0a42a42adbc66a78bac09dab92aaa
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-25 10:44:40 +0700

    :sparkles: home page

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 var express = require('express')
 var app = express()
+const axios = require('axios')
 const PORT = process.env.PORT || 3000
 
-const { userRoutes } = require('./routes')
+const { userRoutes, homeRoutes } = require('./routes')
 const { AuthController } = require('./controllers')
 
 app.set('view engine', 'ejs')
@@ -12,6 +13,18 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/signup', AuthController.signUpForm)
 app.post('/signup', AuthController.signUp)
 
+app.get('/search', (req, res) => {
+  axios
+    .get(`https://api.deezer.com/search?q=${req.query.q}`)
+    .then(data => {
+      res.send(data.data)
+    })
+    .catch(err => {
+      res.send(err.message)
+    })
+})
+
+app.use('/', homeRoutes)
 // app.use('/users', userRoutes)
 
 app.listen(PORT, console.log(`Server runs on PORT ${PORT}`))
---
+++ controllers/HomeController.js
@@ -0,0 +1,19 @@
+const axios = require('axios')
+
+class HomeController {
+  static index(req, res) {
+    axios
+      .get(`https://api.deezer.com/chart/0/tracks`)
+      .then(data => {
+        res.render('home/index', {
+          title: 'Home',
+          topTracks: data.data,
+        })
+      })
+      .catch(err => {
+        res.send(err.message)
+      })
+  }
+}
+
+module.exports = HomeController
