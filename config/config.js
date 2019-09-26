2eaab8f9d9bc6b9f56c3217e41e25dbc0280c1e4
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-26 20:41:40 +0700

    :sparkles:

--- config/config.js
+++ config/config.js
@@ -6,14 +6,14 @@ module.exports = {
     host: '127.0.0.1',
     dialect: 'postgres',
   },
-  test: {
-    username: root,
-    password: null,
-    database: database_test,
-    host: '127.0.0.1',
-    dialect: mysql,
-    operatorsAliases: false,
-  },
+  // test: {
+  //   username: root,
+  //   password: null,
+  //   database: database_test,
+  //   host: '127.0.0.1',
+  //   dialect: mysql,
+  //   operatorsAliases: false,
+  // },
   production: {
     username: 'euauvrbahzvxbb',
     password: 'd762eb4afb2afd9dd9eae3410e391353d49000a31f7db5e3192621a013a94ec6',
