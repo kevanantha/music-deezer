a039d2a8de851563160d8ed06626c70e9bf44053
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-27 10:22:56 +0700

    :anchor:

--- routes/playlistRoutes.js
+++ routes/playlistRoutes.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
 const { PlaylistController } = require('../controllers')
 
-router.get('/playlist', PlaylistController.findAll)
-router.get('/playlist/:SongId', PlaylistController.delete)
+const loginMiddleware = (req, res, next) => {
+  if (req.session.user) {
+    next()
+  } else {
+    res.redirect(`/signin?err=You need to sign in before add playlist`)
+  }
+}
+
+router.get('/playlist', loginMiddleware, PlaylistController.findAll)
+router.get('/playlist/:SongId', loginMiddleware, PlaylistController.delete)
 module.exports = router
