44470af099dd892545f48b0bcf1fcc94d80c1f31
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-27 21:25:55 +0700

    :anchor:

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -7,7 +7,7 @@ router.post('/signup', AuthController.signUp)
 router.get('/signin', AuthController.signInForm)
 router.post('/signin', AuthController.signIn)
 router.get('/signout', AuthController.signOut)
-router.post('/', HomeController.search)
+router.post('/search', HomeController.search)
 
 router.get('/', HomeController.index)
 router.post('/users/add-to-playlist', loginMiddleware, HomeController.addToPlaylist)
